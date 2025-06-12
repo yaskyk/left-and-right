d120ef69298e717903200c35df91c185f4a76c8f
yaskyk <ismaplhau@gmail.com>
2025-06-12 17:56:31 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'metronomo-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'sounds/left.mp3',
+  'sounds/right.mp3',
+  'icon.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
